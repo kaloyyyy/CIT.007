352ca6ae26e7540c49e377e6148ccc650ac89541
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-04-26 13:59:17 +0800

    browse page added

--- groupProject/src/browse/browse.php
+++ groupProject/src/browse/browse.php
@@ -6,12 +6,13 @@
     <meta charset="UTF-8">
     <title>browse</title>
     <link rel="stylesheet" href="../../public/css/grid.css">
+    <link rel="stylesheet" href="../../public/css/chat.css">
 </head>
 <body>
 <?php include_once __DIR__ . '/../../public/header.php' ?>
 <main>
     <section class="basic-grid">
-        <div class="card">
+        <div class="card writing">
             <div class="portfolio" id="hugh">
                 <h6 class="user-name">Hugh#0211</h6>
                 <h6 class="comm-type portfolio">Academic Writing</h6>
@@ -28,19 +29,57 @@
             </div>
             <div class="svg-group">
                 <div class="div-svg">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                        <path d="M224 128L224 0H48C21.49 0 0 21.49 0 48v416C0 490.5 21.49 512 48 512h288c26.51 0 48-21.49 48-48V160h-127.1C238.3 160 224 145.7 224 128zM96 224c17.67 0 32 14.33 32 32S113.7 288 96 288S64 273.7 64 256S78.33 224 96 224zM318.1 439.5C315.3 444.8 309.9 448 304 448h-224c-5.9 0-11.32-3.248-14.11-8.451c-2.783-5.201-2.479-11.52 .7949-16.42l53.33-80C122.1 338.7 127.1 336 133.3 336s10.35 2.674 13.31 7.125L160 363.2l45.35-68.03C208.3 290.7 213.3 288 218.7 288s10.35 2.674 13.31 7.125l85.33 128C320.6 428 320.9 434.3 318.1 439.5zM256 0v128h128L256 0z"/>
-                    </svg>
+                    <?php include __DIR__ . "/../../public/svg/pictures.svg" ?>
                 </div>
                 <div class="div-svg">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path d="M256 31.1c-141.4 0-255.1 93.12-255.1 208c0 49.62 21.35 94.98 56.97 130.7c-12.5 50.37-54.27 95.27-54.77 95.77c-2.25 2.25-2.875 5.734-1.5 8.734c1.249 3 4.021 4.766 7.271 4.766c66.25 0 115.1-31.76 140.6-51.39c32.63 12.25 69.02 19.39 107.4 19.39c141.4 0 255.1-93.13 255.1-207.1S397.4 31.1 256 31.1zM127.1 271.1c-17.75 0-32-14.25-32-31.1s14.25-32 32-32s32 14.25 32 32S145.7 271.1 127.1 271.1zM256 271.1c-17.75 0-31.1-14.25-31.1-31.1s14.25-32 31.1-32s31.1 14.25 31.1 32S273.8 271.1 256 271.1zM383.1 271.1c-17.75 0-32-14.25-32-31.1s14.25-32 32-32s32 14.25 32 32S401.7 271.1 383.1 271.1z"/>
-                    </svg>
+                        <?php include __DIR__ . "/../../public/svg/chat.svg" ?>
                 </div>
             </div>
         </div>
-        <div class="card">2</div>
-        <div class="card">3</div>
+        <div class="card coding">
+            <div class="portfolio" id="hak16">
+                <h6 class="user-name">Hak_16#6916</h6>
+                <h6 class="comm-type portfolio">Programming and coding</h6>
+                <h6 class="comm-rate portfolio">starts at 200 php</h6>
+                <h6 class="credentials portfolio">Languages:
+                    <ul>
+                        <li>C++</li>
+                        <li>Java</li>
+                        <li>Javascript: node.js, Jquery, discord.js</li>
+                        <li>Web App development: <br>HTML, CSS, JS, PHP, MYSQL</li>
+                        <li>Flowcharts && Pseudocode</li>
+                        <li>C-Based QMK Firmware</li>
+                    </ul>
+                </h6>
+            </div>
+            <div class="svg-group">
+                <div class="div-svg">
+                    <?php include __DIR__ . "/../../public/svg/pictures.svg" ?>
+                </div>
+                <div class="div-svg">
+                    <?php include __DIR__ . "/../../public/svg/chat.svg" ?>
+                </div>
+            </div>
+        </div>
+        <div class="card photo">
+            <div class="portfolio" id="wokes">
+                <h6 class="user-name">Wokes#46549</h6>
+                <h6 class="comm-type portfolio">Photography and editing</h6>
+                <h6 class="comm-rate portfolio">Photo shoot package at 3000 php</h6>
+                <Ul>
+                    <li>20 edited photos (selected by the photographer and client)</li>
+                    <li>unlimited number of shots</li>
+                </Ul>
+            </div>
+            <div class="svg-group">
+                <div class="div-svg">
+                    <?php include __DIR__ . "/../../public/svg/pictures.svg" ?>
+                </div>
+                <div class="div-svg">
+                    <?php include __DIR__ . "/../../public/svg/chat.svg" ?>
+                </div>
+            </div>
+        </div>
         <div class="card">4</div>
         <div class="card">5</div>
         <div class="card">6</div>
@@ -49,5 +88,18 @@
         <div class="card">9</div>
     </section>
 </main>
+<button class="open-button" onclick="openForm()">Chat</button>
+<div class="chat-popup" id="myForm">
+    <form action="/action_page.php" class="form-container">
+        <h1>Chat</h1>
+
+        <label for="msg"><b>Message</b></label>
+        <textarea placeholder="Type message.." name="msg" required></textarea>
+
+        <button type="submit" class="btn">Send</button>
+        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+    </form>
+</div>
+<script src="../../public/script/chatPopUp.js"></script>
 </body>
 </html>
