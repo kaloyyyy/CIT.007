1ce8d70d03153d343133004affe0743eee72c772
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-05-03 20:40:49 +0800

    Update user_index.php

--- groupProject/src/user/user_index.php
+++ groupProject/src/user/user_index.php
@@ -7,11 +7,6 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
-
-function runMyFunction() {
-
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,26 +30,74 @@ function runMyFunction() {
             <?php date_default_timezone_set("Asia/Manila"); ?>
         </div>
     </div>
-    <div id="test">
-        <div id="active"><?php $modID = $_GET['mod'];
-            echo $modID ?>
-        </div>
-        <script>
-            let testCount = 0;
-            let modID = $("#active").text();
-            console.log("yes");
-            console.log(modID);
-
-            //chatList.php - list all chats and messages.
-            $("#test").load("chatList.php", {testNewCount: testCount, modID: modID});
-            console.log("log");
-
-        </script>
+    <div class="chat-display">
+        <ul>
+            <?php
+            $userID = require __DIR__ . '/../convo-query/user-query.php';
+            $convoQue = "select * from convo where userID = $userID";
+            $convoRes = mysqli_query($mysqli, $convoQue);
+            $convoRow = mysqli_fetch_assoc($convoRes);
+            foreach ($convoRes as $convoRow) {
+                $convoID = $convoRow['convoID'];
+                echo "<li>";
+                $modID = $convoRow['modID'];
+                $modQue = "select * from mods where modID = $modID";
+                $modRes = mysqli_query($mysqli, $modQue);
+                $modRow = mysqli_fetch_assoc($modRes);
+                $modUsername = $modRow['modUsername'];
+                if (isset($_GET['mod'])) {
+                    $modID = $modRow['modID'];
+                    if ($modID == $_GET['mod']) {
+                        echo "<a href='/groupProject/src/user/user_index.php?mod=$modID' class='current-chat'>" . $modUsername . "</a><br>";
+                    } else {
+                        echo "<a href='/groupProject/src/user/user_index.php?mod=$modID'>" . $modUsername . "</a><br>";
+                    }
+                } else {
+                    echo "<a href='/groupProject/src/user/user_index.php?mod=$modID'>" . $modUsername . "</a><br>";
+                }
+                echo "</li>";
+            }
+            echo "</ul>";
+            ?>
+            <div class="chat-box">
+                <div id="chat-refresh">
+                    <div id="active"><?php $modID = $_GET['mod'];
+                        echo $modID ?>
+                    </div>
+                </div>
+                <div class="send-section">
+                    <div class=" chat-form">
+                        <input type="text" name="chat" class="chat-input" id="chat-message">
+                        <button name="send" id="chat-send" onclick="chatClick()"
+                                class="button-send-chat fa-solid fa-circle-chevron-right send-chat">
+                    </div>
+                </div>
+            </div>
     </div>
+    <div id="trychat">
+
+    </div>
+    <script>
+        let testCount = 0;
+        let modID = $("#active").text();
+        function chatClick() {
+            let chat = $('#chat-message').val();
+            console.log(chat);
+            $('#trychat').load("/groupProject/src/convo-query/chat-insert.php", {chat: chat, modID: modID});
+            $('#chat-message').val("");
+        }
+
+        console.log("yes");
+        console.log(modID);
+        //chatList.php - list all chats and messages.
+        $("#chat-refresh").load("chatList.php", {testNewCount: testCount, modID: modID});
+        console.log("log");
+    </script>
 
 </main>
 </body>
 <script>
+
     //script for polling then calls the chatList.php
     testCount = 1;
     (function loop() {
@@ -62,7 +105,7 @@ function runMyFunction() {
             // Your logic here
             testCount = testCount + 1;
             console.log("log");
-            $("#test").load("chatList.php", {testNewCount: testCount, modID: modID});
+            $("#chat-refresh").load("chatList.php", {testNewCount: testCount, modID: modID});
             loop();
         }, 1500);
     })();
