b668f30977d9fb879969ddf1afbe872396aa5629
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-05-03 20:40:53 +0800

    Update convo-query.php

--- groupProject/src/convo-query/convo-query.php
+++ groupProject/src/convo-query/convo-query.php
@@ -2,7 +2,8 @@
 // Include config file
 require_once __DIR__ . "/../../config/config.php";
 require __DIR__ . '/../convo-query/user-query.php';
-$convoQue = "select * from convo where userID = $userID";
+require __DIR__ . '/../convo-query/mod-query.php';
+$convoQue = "select * from convo where userID = $userID and modID = $modID";
 $convoRes = mysqli_query($mysqli, $convoQue);
 $convoRow = mysqli_fetch_assoc($convoRes);
 $convoID = $convoRow['convoID'];
