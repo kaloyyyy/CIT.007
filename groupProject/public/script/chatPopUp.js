d40ea3360aa9976d4a3047711d2cb039555dc81f
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-05-03 10:07:53 +0800

    Update chatPopUp.js

--- groupProject/public/script/chatPopUp.js
+++ groupProject/public/script/chatPopUp.js
@@ -1,7 +1,17 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
+(function() {
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+    $('#live-chat header').on('click', function() {
+
+        $('.chat').slideToggle(300, 'swing');
+        $('.chat-message-counter').fadeToggle(300, 'swing');
+
+    });
+
+    $('.chat-close').on('click', function(e) {
+
+        e.preventDefault();
+        $('#live-chat').fadeOut(300);
+
+    });
+
+}) ();
