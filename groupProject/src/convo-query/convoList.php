51b538aad386faa2efb92422a0cf6781fd59cc33
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-05-22 17:24:06 +0800

    Update convoList.php

--- groupProject/src/convo-query/convoList.php
+++ groupProject/src/convo-query/convoList.php
@@ -4,24 +4,46 @@ require_once __DIR__ . '/../../config/config.php';
 $currID = $_SESSION['id'];
 $chatFind = $_SESSION['tableAccount'];
 $idFind = $_SESSION['idFind'];
+$myCol = $_SESSION['myCol'];
+$currentChat = $_POST['chatID'];
+if (is_numeric($currentChat)){
+    if ($_SESSION['mod']) {
+        $modID = $currID;
+        $userID = $currentChat;
+    } else {
+        $userID = $currID;
+        $modID = $currentChat;
+    }
+    $currChatCheckQue = "select * from convo where $myCol = $currID and $idFind = $currentChat";
+    $currChatCheckRes = $mysqli->query($currChatCheckQue);
+    if (mysqli_num_rows($currChatCheckRes) == 0) {
+        $convoInsert ="INSERT INTO komision.convo (convoID, userID, modID)
+    VALUES (NULL, '$userID', '$modID');";
+        $mysqli->query($convoInsert);
+    }
 
-$convoQue = "select * from convo where userID = $currID";
+}
+$convoQue = "select * from convo where $myCol = $currID";
 $convoRes = $mysqli->query($convoQue);
 $convoRow = mysqli_fetch_assoc($convoRes);;
 
-$currentChat = $_POST['chatID'];
 foreach ($convoRes as $convoRow) {
     $convoID = $convoRow['convoID'];
-    echo "<li>";
-    $connectID = $convoRow['modID'];
+
+    $connectID = $convoRow[$idFind];
     $connectQue = "select * from $chatFind where $idFind = $connectID";
     $connectRes = $mysqli->query($connectQue);
     $connectRow = mysqli_fetch_assoc($connectRes);
     $connectUsername = $connectRow['username'];
     if ($connectID == $currentChat) {
-        echo "<a href='/groupProject/src/user/user_index.php?chatID=$connectID' id='current-chat'>" . $connectUsername . "</a><br>";
+        echo "<a href='/groupProject/src/user/user_index.php?chatID=$connectID'  >";
+        echo "<li id='current-chat'>";
+        echo $connectUsername . "</li></a>";
+
     } else {
-        echo "<a href='/groupProject/src/user/user_index.php?chatID=$connectID'>" . $connectUsername . "</a><br>";
+        echo "<a href='/groupProject/src/user/user_index.php?chatID=$connectID'  >";
+        echo "<li>";
+        echo $connectUsername . "</li></a>";
     }
-    echo "</li>";
+
 }
