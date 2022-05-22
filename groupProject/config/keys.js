b5d7ae83dc2db6dcca536c892835a7ddf586db81
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-05-23 01:22:25 +0800

    rebuild komision with nodeJS and react

---
+++ groupProject/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb+srv://kal:austria16@cluster0.ypacv.mongodb.net/?retryWrites=true&w=majority"
+};
+module.exports = {
+    mongoURI: "mongodb+srv://kal:austria16@cluster0.ypacv.mongodb.net/?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
