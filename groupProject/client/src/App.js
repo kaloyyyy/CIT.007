09d9cfd740b161b6a11b0e1de7dbf21759dc62f9
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-08-11 08:32:13 +0800

    App.js update

--- groupProject/client/src/App.js
+++ groupProject/client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 class App extends Component {
   render() {
     return (
         <div className="App">
-          <h1>arosintsriotenarstneirstyu;lqwfpioerasnt</h1>
+          <Navbar/>
         </div>
     );
   }
