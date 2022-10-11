62de7df899a36996c6118f870fed06bf44439d06
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-10-11 12:11:29 +0800

    post request backend done

--- komision/src/post-request/index.php
+++ komision/src/post-request/index.php
@@ -4,35 +4,55 @@ chdir(dirname(__DIR__));
 include __DIR__ . '/../../config/meta.php';
 include __DIR__ . '/../../public/header.php';
 ?>
-    <div style="width: 220px;height: 30px;background: white" xmlns="http://www.w3.org/1999/html">
-        <input class="inputRequest" id="reqDesc" style="width: 220px;height: 30px; color: black; resize: none" onclick="editClick(id)"
+    <div class="flex-row" style="width: 320px;height: auto;background: white; color: black" xmlns="http://www.w3.org/1999/html">
+        Requirement info:
+        <input class="inputRequest" id="reqDesc" style="width: 300px;height: 30px; color: black; resize: none" onclick="editClick(id)"
                onfocusout="resetClick(id,'what\'s your requirement?')"
                placeholder=
                "what's your requirement?">
+        <br>
+        Price:<br>
         <input class="inputRequest" type="number"
-               id="reqPrice" style="width: 220px;height: 30px; color: black; resize: none" onclick="editClick(id)"
+               id="reqPrice" style="width: 300px;height: 30px; color: black; resize: none" onclick="editClick(id)"
                onfocusout="resetClick(id,'what\'s your budget?')"
                placeholder=
                "what's your budget?">
-        <button type="button" onclick="submitReq()" style="background: white">
+        <br>
+        Date & Time of Deadline:<br>
+        <div class="flex justify-content-start" style="width: 300px">
+            <input type="text" onfocus="(this.type='date')" onfocusout="(this.type ='text')" id="deadline" placeholder="due date" style="width:
+            200px">
+            <input type="text" onfocus="(this.type='time')"  onfocusout="(this.type ='text')" id="timeDl" placeholder="time" style="width: 100px">
+        </div>
+
+        <button type="button" onclick="submitReq()" style="background: pink">
             test
         </button>
 
-        <div class="send"></div>
+        <div class="send"></div
     </div>
 
     <script>
         let selectDesc = $("#reqDesc")
         let selectPrice = $("#reqPrice")
+        let selectDate = $("#deadline")
+        let selectTime = $("#timeDl");
 
         function submitReq() {
             let desc = selectDesc.val();
             let price = selectPrice.val();
-            $(".send").load('post-request-query.php', {desc: desc, price: price})
-            console.log('post')
+            let date = selectDate.val();
+            let time = selectTime.val();
+            let deadline = date + " " +time;
+            console.log(deadline);
+            if (desc && price && date && time !== '')
+            $(".send").load('post-request-query.php', {
+                desc    : desc,
+                price   : price,
+                deadline: deadline,
+            })
 
         }
-
 
 
         function editClick(id) {
--- komision/src/post-request/post-request-query.php
+++ komision/src/post-request/post-request-query.php
@@ -3,9 +3,10 @@ require_once __DIR__ . "/../../config/config.php";
 session_start();
 $desc = $_POST['desc'];
 $price = $_POST['price'];
+$datetime = $_POST['deadline'];
 $currID = $_SESSION['id'];
-echo $price . $desc;
 
-$query = "INSERT INTO komision.postrequest (postID, userID, Description, price, date_created) 
-            VALUES (DEFAULT, $currID, '$desc', $price, DEFAULT);";
+
+$query = "INSERT INTO komision.postrequest (postID, userID, Description, price, date_created, deadline) 
+            VALUES (DEFAULT, $currID, '$desc', $price, DEFAULT, '$datetime');";
 $mysqli->query($query);
