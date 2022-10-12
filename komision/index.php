20e84d3e1a57411a9354089f83d96bad11b50e3e
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-10-12 19:50:10 +0800

    sidebar added

--- komision/index.php
+++ komision/index.php
@@ -3,13 +3,3 @@ require_once 'config/config.php';
 require_once 'config/meta.php';
 session_start();
 require_once 'public/index.php';
-if ($_SESSION['loggedin']){
-    echo "naka login ka na";
-    if ($_SESSION['accType']==0){
-        echo "<br>user";
-    }else if($_SESSION['accType']==1){
-        echo "<br>free lancer";
-    }else{
-        echo "<br>admin";
-    }
-}
