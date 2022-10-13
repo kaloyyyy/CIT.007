450f7513e9bb9acd00c5ad45255dc1b91f5c44b8
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-10-13 15:10:06 +0800

    Update index.php

--- komision/index.php
+++ komision/index.php
@@ -1,19 +1,35 @@
 <?php
+chdir(dirname(__DIR__));
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+<head>
+    <?php include __DIR__ . '/config/meta.php';
+    ?>
+    <title>Komi-sion</title>
+</head>
+<?php
 require_once 'config/config.php';
 require_once 'config/meta.php';
 if (!isset($_SESSION)) {
     session_start();
 }
-$user = "src/user/login.php";
+$login = "src/user/login.php";
+$register ='src/user/register.php';
 $home = 'public/index.php';
-
+$logged ='src/user/logged-in.php';
+$posting = 'src/post-request/index.php';
+require $home;
 if (isset($_SESSION['id'])) {
-    require $home;
+    require $logged;
+    require $posting;
 } else {
-    require $home;
+    require $login;
+    require $register;
 }
 
-
+?>
+</html>
 
 
 
