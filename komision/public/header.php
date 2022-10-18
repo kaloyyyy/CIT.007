291ab5ca66e4ceb07795e44cbc92bccf4c123903
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-10-19 01:06:08 +0800

    Update header.php

--- komision/public/header.php
+++ komision/public/header.php
@@ -1,14 +1,21 @@
-<div class="flex-row align-content-between navbar komi-header">
-    <h5>
-        <?php
-        if(isset($currentPage)){
-            echo $currentPage;
-        }else{
-            echo "Komi-sion";
-        }
-        ?>
-    </h5>
-</div>
+<?php
+if (isset($currentPage)) {
+    if ($currentPage == 'Messages') {
+
+    }else{
+        echo "<div class='flex-row align-content-between navbar komi-header'>
+            <h5>";
+        echo $currentPage;
+        echo "</h5></div>";
+    }
+
+} else {
+    echo "<div class='flex-row align-content-between navbar komi-header'>
+            <h5>";
+    echo "Komision";
+    echo "</h5></div>";
+}
+?>
 <script>
 
 </script>
