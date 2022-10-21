2f95201020fa516e1616a5f267836a2b6c25256d
RanJiko <ranjiko30@gmail.com>
2022-10-21 22:47:38 +0800

    JS toggle hide /open
    JS toggle hide under req info
    
    toggle once
    if iback = refresh page.. so can toggle again(not sure/try try lng)

--- komision/public/index.php
+++ komision/public/index.php
@@ -1,3 +1,25 @@
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">    
+
+</head>
+<script>
+
+$(document).ready(function(){
+    
+  $('#toggle') .one ("click", function(){
+    $('.ftoggle').toggle ('normal');
+    
+    
+
+  });
+});
+</script>
+
+
+
+
+
+
 <body class=" darktheme ">
 <div class="container ">
     <div class="">
@@ -15,7 +37,7 @@
 
                 ?>
                 <?php
-                $sample = "<div class=' border-top post-border border-bottom px-3 py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                $sample = "<div class='border-top post-border border-bottom px-3 py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                 veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
@@ -45,11 +67,24 @@
                     }
                 } else {
                     include 'header.php';
+<<<<<<< Updated upstream
                     echo "<div class=' border-top post-border border-bottom px-3 py-2'>
                             <div class='container d-flex justify-content-center'>
+=======
+                    echo "<div  class='border-top post-border border-bottom px-3 py-2'>
+                            <div class='container d-flex justify-content-center' >
+>>>>>>> Stashed changes
                             <div class='flex-row' xmlns=\"www.w3.org/1999/html\">
                             <div class='my-2 justify-content-center align-items-center'>
+                           
+                            
+
+                        <div class='text-right'>
+                        <button type='button' id ='backToggle' class='btn btn-outline-light ftoggle'><span class='bi bi-arrow-left' onClick='window.location.reload()';></span></button>
+                            
+                        </div>
                         Requirement info:<br>
+<<<<<<< Updated upstream
                          <textarea class='form-control' id='toggle' aria-label='With textarea' style='width:
                         300px; height: 90px; text-overflow: clip; resize: none'; placeholder='Whats on your mind, User?' 
                         onfocus='$('#lowbox').show();'
@@ -59,18 +94,32 @@
 
                         <div id='lowbox' style='display:none'>
                           <div class='my-2'>
+=======
+                         <textarea  class='form-control' id='toggle' aria-label='With textarea' style='width:
+                        300px; height: 90px; text-overflow: clip; resize: none'; placeholder='Whats on your mind, User?' onclick='editClick(id)'
+                         onfocusout='resetClick (id,'Whats on your mind, User?')'></textarea>
+
+                        </div>
+
+                          <div class='my-2 '>
+                     <div class='ftoggle'>
+>>>>>>> Stashed changes
                         Price:<br>
-                        <input class='inputRequest' type='number'
+                        </div>
+
+                        <input class=' inputRequest ftoggle'  type='number'
                                id='reqPrice' style='width: 300px;height: 30px; color: black; resize: none' onclick='editClick(id)''
                                onfocusout='resetClick(id,'what\'s your budget?')'
-                               placeholder='What is your budget?'>
+                               placeholder='What is your budget?' >
                     </div>
 
                      <div class='my-2'>
+                     <div class='ftoggle'>
                         Date & Time of Deadline:<br>
+                        </div>
                         <div class='flex justify-content-start' style='width: 300px '>
-                            <input type='text' onfocus='(this.type='date')' onfocusout='(this.type ='text')' id='deadline' placeholder='due date'
-                                   style='width:180px'><input type='text' onfocus='(this.type='time')' onfocusout='(this.type ='text')' id='timeDl'
+                            <input class='ftoggle' type='text' onfocus='(this.type='date')' onfocusout='(this.type ='text')' id='deadline' placeholder='due date'
+                                   style='width:180px'><input class='ftoggle'type='text' onfocus='(this.type='time')' onfocusout='(this.type ='text')' id='timeDl'
                                                               placeholder='time'
                                                               style='width: 120px'>
                         </div>
@@ -78,8 +127,8 @@
                     </div>
                     <div class=' border-top post-border px-3 py-3'>
                      <div class='my-2 text-right'>
-                        <button type='button' class = 'btn btn-light' on click='submitReq()'>
-                            Post
+                        <button type='button'  class = 'btn btn-outline-light ftoggle'  on click='submitReq()'>
+                            Posts
                         </button>
                     </div>
                     </div>
