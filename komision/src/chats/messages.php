7bf96b8ea6450488ca16a5dd1e1c1504b6f24be8
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-10-27 18:21:51 +0800

    update messages.php

--- komision/src/chats/messages.php
+++ komision/src/chats/messages.php
@@ -1,31 +1,64 @@
-<div class="container flex-column ">
+<div class="container flex-column px-0">
     <?php
     session_start();
     require_once __DIR__ . '/../../config/config.php';
     $username = $_SESSION['username'];
     $userID = $_SESSION['id'];
-    echo "<div class=' flex-column d-flex align-items-end' style='height: inherit'>";
+    $chatID = $_POST['chatID'];
+    $accType = $_SESSION['accType'];
+    echo "<div class=' flex-column d-flex align-items-center' style='height: inherit'>";
     if ($_POST['chatID'] !== null) {
         $currentChat = $_POST['chatID'];
         if (is_numeric($currentChat)) {
-            $chatsMessagesQue = "SELECT * FROM convo join chat c on convo.convoID = c.convID where userID = $userID && convo.fl_userID = $currentChat";
+            $chatsMessagesQue = "SELECT * FROM convo join chat c on convo.convoID = c.convID join freelancers f on f.freelancerID = convo.fl_userID join users u on u.userID = f.freelancerID where convo.convoID = $chatID order by c.created_at ASC ";
             $chatsMessagesResult = $mysqli->query($chatsMessagesQue);
             $chatsMessagesRow = mysqli_fetch_assoc($chatsMessagesResult);
             $previousSender = null;
             foreach ($chatsMessagesResult as $chats) {
                 $message = $chats['message'];
-                printChat($message, "yes", '0');
+                $sentBy = $chats['sender'];
+                if ($sentBy == $accType) {
+                    $senderName = $username;
+                } else {
+                    $senderName = $chats['username'];
+                }
+                if ($previousSender == $sentBy) {
+                    $newChatSection = false;
+                } else {
+                    $newChatSection = true;
+                }
+                $previousSender = $sentBy;
+                printChat($message, $senderName, $sentBy, $newChatSection);
             }
         }
     }
     echo "</div>";
-    function printChat($message, $senderName, $sentBy): void
+    function printChat($message, $senderName, $sentBy, $join): void
     {
+        $accType = $_SESSION['accType'];
+        if ($sentBy == $accType) {
+            $bgcolor = 'dark-main';
+            echo "
+            <div class='d-flex container-fluid flex-column justify-content-end align-items-end px-1'>
+            ";
+        } else {
+            $bgcolor = 'bg-secondary';
+            echo "<div class='d-flex flex-column container-fluid justify-content-start align-items-start px-1'>
+            ";
+        }
+        if ($join) {
+            echo "<span class='text-secondary'>$senderName</span>";
+            $marginY = '';
+        } else {
+            $marginY = '';
+        }
         echo "
-    <div class='card text-primary' style='width: fit-content'>
-        $message
-    </div>
+            <div class='card  px-1 py-1 $bgcolor $marginY' style='width: fit-content; max-width: 69%'>
+                $message
+            </div>
+        </div>
     ";
+
     }
 
     ?>
