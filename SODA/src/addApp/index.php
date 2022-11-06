1ae08dc6eb73a064b7c63db146c407ea312288b7
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-11-07 05:48:19 +0800

    add appointment update

---
+++ SODA/src/addApp/index.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once('config/config.php');
+require_once('config/meta.php');
+
+
+?>
+
+<html lang="en">
+<head>
+    <title>SODA | Schedule</title>
+    <style>
+
+    </style>
+</head>
+<body>
+<div class='d-flex flex-column justify-content-center align-items-center'>
+    <div class="border-bottom" style="width: 690px">
+        <div id="doctorList" class="w-100 d-flex">
+
+        </div>
+    </div>
+    <input class="d-none" value="" id="doc_id">
+    <div class="wrapper">
+        <form>
+            <div class="form-group">
+                <label>What is the Appointment for?</label>
+                <input type="text" name="desc"
+                       class="form-control"
+                       value="" id="desc">
+                <label>Date and Time</label>
+                <input type="date" name="date"
+                       class="form-control"
+                       value="" id="date">
+                <span class="invalid-feedback"></span>
+                <input type="time" name="time"
+                       class="form-control"
+                       value="" id="time">
+            </div>
+            <div class="form-group">
+                <input type="button" class="btn btn-primary" value="Submit" onclick="addAppoint()">
+            </div>
+        </form>
+    </div>
+    <div id="addAppoint">
+    </div>
+</div>
+
+
+</body>
+<script>
+    $('#doctorList').load('/soda/src/look/doctors.php')
+
+    function addDocID(d_id) {
+        $("#doc_id").val(d_id);
+    }
+
+    function selectDoc(d_id) {
+        console.log(d_id)
+        $(".docList").removeClass('docActive')
+        $('#' + d_id).addClass('docActive')
+        addDocID(d_id);
+    }
+
+    function addAppoint() {
+        let d_id = $("#doc_id").val();
+        let date = $('#date').val();
+        let time = $('#time').val();
+        let desc = $('#desc').val();
+        console.log(time);
+
+        $('#addAppoint').load('/soda/src/addApp/add.php', {
+            d_id: d_id,
+            date: date,
+            time: time,
+            desc: desc
+        })
+    }
+</script>
+</html>
+
