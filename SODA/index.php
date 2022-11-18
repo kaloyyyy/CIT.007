11dd77162c50df1f8d6ac2190a2c31b70dec9968
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-11-19 01:11:16 +0800

    index.php update

--- SODA/index.php
+++ SODA/index.php
@@ -5,7 +5,7 @@
     require_once('config/config.php');
     require_once('config/meta.php');
     ?>
-    <title>SODA</title></head>
+</head>
 </html>
 <body class="h-100" style="">
 <div class=" container-fluid m-0 p-0 h-100">
@@ -16,48 +16,48 @@
         session_start();
     }
     //--Switcher upper for detecting directory
-    if (isset($_GET['page'])) {
-        $page = $_GET['page'];
-    } else {
-        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
-            $page = 'home';
-        } else {
-            $page = 'landing';
-        }
 
+
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
+        $page = $_GET['page'] ?? 'home';
+        require_once __DIR__ . '/components/header.php';
+
+    } else {
+        $page = $_GET['page'] ?? 'landing';
     }
 
     switch ($page) {
         case 'landing':
-            require __DIR__ . '/src/landing/welcome.php';
+            require_once __DIR__ . '/src/landing/welcome.php';
+            echo "<title>SODA | Welcome</title>";
             break;
         case 'home':
-            include __DIR__ . '/components/header.php';
-            require __DIR__ . '/src/users/index.php';
+
+            require_once __DIR__ . '/src/users/index.php';
+            echo "<title>SODA | Home</title>";
             break;
         case 'login':
         case 'user':
-            require __DIR__ . '/src/users/index.php';
+            require_once __DIR__ . '/src/users/index.php';
             break;
         case 'logout':
-
-            require __DIR__ . '/src/users/logout.php';
+            require_once __DIR__ . '/src/users/logout.php';
             break;
         case 'viewApp':
-            include __DIR__ . '/components/header.php';
-            require __DIR__ . '/src/viewApp/index.php';
+            require_once __DIR__ . '/src/viewApp/index.php';
             break;
         case 'addApp':
-            include __DIR__ . '/components/header.php';
-            require __DIR__ . '/src/addApp/index.php';
+            require_once __DIR__ . '/src/addApp/index.php';
             break;
         case 'updateApp':
-            include __DIR__ . '/components/header.php';
-            require __DIR__ . '/src/updateApp/index.php';
+            require_once __DIR__ . '/src/updateApp/index.php';
+            break;
+        case 'addProfile':
+            require_once __DIR__ . '/src/addProfile/index.php';
             break;
         default:
             http_response_code(404);
-            require __DIR__ . '/404.php';
+            require_once __DIR__ . '/404.php';
             break;
     }
     require_once __DIR__ . '/components/waves.php';
