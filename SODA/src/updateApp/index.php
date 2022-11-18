7b1baaccd003864553178c8b328e6b4ccfe19672
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-11-19 01:10:38 +0800

    update Appointment

--- SODA/src/updateApp/index.php
+++ SODA/src/updateApp/index.php
@@ -7,60 +7,48 @@ $id = $_SESSION['id'];
 
 <html lang="en">
 <head>
-    <title>SODA | Appointments</title>
+    <title>SODA | Update</title>
 </head>
 <body>
 <div class='d-flex flex-column justify-content-center align-items-center'>
     <h4>click the appointment you want to edit</h4>
     <div id="viewApp">
-        <?php
-        $query = "select * from appointments join doctor d on d.d_id = appointments.d_id where p_id = $id order by ap_date";
-        $result = $mysqli->query($query);
-        $rows = mysqli_fetch_assoc($result);
 
-        foreach ($result as $row) {
-            $d_id = $row['app_id'];
-            $desc = $row['desc'];
-            $datetime = explode(" ", $rows['ap_date']);
-            echo "<div id='$d_id' class='border m-1 p-1 appList rounded editApp' style='width: 420px' onclick='editNow(this.id)'>";
-            echo "<div class='subAppList ' >";
-            echo $desc . "<br>";
-            echo "Dr. " . $row['first_name'] . " " . $row['sur_name'] . "<br>";
-            echo "Appointment Schedule is at " . $row['ap_date'];
-            echo "<div>";
-            echo "<div class='wrapper d-none' id='hide$d_id'>
-                    <form>
-                    <div class='form-group'>
-                    <label>What is the Appointment for?</label>
-                    <input type='text' name='desc'
-                           class='form-control'
-                           value='$desc' id='desc'>
-                    <label>Date and Time</label>
-                    <input type='date' name='date'
-                           class='form-control'
-                           value='$datetime[0]' id='date'>
-                    <span class='invalid-feedback'></span>
-                    <input type='time' name='time'
-                           class='form-control'
-                           value='$datetime[1]' id='time'>
-                    </div>
-                    <div class='form-group'>
-                        <input type='button' class='btn btn-primary' value='Submit' onclick='submitEdit()'>
-                        <input type='button' class='btn btn-warning' value='Cancel Appointment' onclick='cancelApp()'>
-                    </div>
-                </form>
-            </div>";
-            echo "</div>";
-            echo "</div>";
-            echo "</div>";
-        }
-        ?>
     </div>
 
-    <input class="d-none" value="" id="app_id">
+    <div class="bg-success m-1 p-1 d-none rounded" id="app_id"></div>
+    <div id="cancel" class="bg-warning m-1 p-1 d-none rounded">
+    </div>
 </body>
 <script>
+    $('#viewApp').load('/src/updateApp/loadApp.php');
+
+    function loadApp() {
+        $('#viewApp').load('/src/updateApp/loadApp.php')
+    }
+
     function cancelApp(id) {
+        console.log(id);
+        $('#app_id').load('/src/updateApp/cancelApp.php', {
+            ap_id: id
+        })
+        $('#viewApp').load('/src/updateApp/loadApp.php');
+        $('#cancel').text("Cancelled Appointment").removeClass('d-none')
+    }
+
+    function submitEdit(app_id) {
+        let date = $('#date-' + app_id).val();
+        let time = $('#time-' + app_id).val();
+        let desc = $('#desc-' + app_id).val();
+        console.log(desc);
+        $('#app_id').load('/src/updateApp/updateApp.php', {
+            app_id: app_id,
+            date  : date,
+            time  : time,
+            desc  : desc
+        })
+        setTimeout(loadApp, 10);
+        $('#app_id').removeClass('d-none')
 
     }
 
@@ -68,7 +56,6 @@ $id = $_SESSION['id'];
         $('.wrapper').addClass('d-none')
         $('.appList').addClass('editApp').removeClass('activeEdit')
         $('#' + id).removeClass('editApp').addClass('activeEdit')
-
         $('#hide' + id).removeClass('d-none ')
     }
 </script>
---
+++ SODA/src/updateApp/loadApp.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+$id = $_SESSION['id'];
+$userType = $_SESSION['userType'];
+if ($userType == 0) {
+    $query = "select * from appointments join doctor d on d.d_id = appointments.d_id where p_id = $id order by ap_date";
+} else {
+    $query = "select * from appointments join profile p on p.p_id = appointments.p_id where d_id = $id order by ap_date";
+}
+require_once(__DIR__ . '/../../config/config.php');
+
+$result = $mysqli->query($query);
+$rows = mysqli_fetch_assoc($result);
+
+foreach ($result as $row) {
+    $app_id = $row['app_id'];
+    $desc = $row['desc'];
+    $datetime = explode(" ", $rows['ap_date']);
+    echo "<div id='$app_id' class='border m-1 p-1 appList rounded editApp' style='width: 420px' onclick='editNow(this.id)'>";
+    echo "<div class='subAppList ' >";
+    echo $desc . "<br>";
+    echo "Dr. " . $row['first_name'] . " " . $row['sur_name'] . "<br>";
+    echo "Appointment Schedule is at " . $row['ap_date'];
+    echo "<div>";
+    echo "<div class='wrapper d-none' id='hide$app_id'>
+                    <form>
+                    <div class='form-group'>
+                    <label>What is the Appointment for?</label>
+                    <input type='text' name='desc'
+                           class='form-control'
+                           value='$desc' id='desc-$app_id'>
+                    <label>Date and Time</label>
+                    <input type='date' name='date'
+                           class='form-control'
+                           value='$datetime[0]' id='date-$app_id'>
+                    <span class='invalid-feedback'></span>
+                    <input type='time' name='time'
+                           class='form-control'
+                           value='$datetime[1]' id='time-$app_id'>
+                    </div>
+                    <div class='form-group'>
+                        <input type='button' class='btn btn-primary' value='Submit' onclick='submitEdit($app_id)'>
+                        <input type='button' class='btn btn-warning' value='Cancel Appointment' onclick='cancelApp($app_id)'>
+                    </div>
+                </form>
+            </div>";
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+}
+?>
