f1706d7642e41c7bc91a5cf4d185f5679796a3c6
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-11-30 13:04:13 +0800

    Create template.php

---
+++ komiV2/src/template/template.php
@@ -0,0 +1,8 @@
+<div class="col-6  post-border border-left border-right  p-0 m-0">
+    <?php
+    require_once __DIR__.'/../../components/header.php';
+    ?>
+    <div>
+        insert content here
+    </div>
+</div>
