50217171bc2323b941f1316b26013f5cd7ff7a90
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-11-30 13:04:20 +0800

    Update index.php

--- komiV2/index.php
+++ komiV2/index.php
@@ -13,7 +13,10 @@
     <div class="row d-flex justify-content-center h-100">
         <?php
 
-
+        function title($title): void
+        {
+            echo "<title>Komision | $title</title>";
+        }
         if (!isset($_SESSION)) {
             session_start();
         }
@@ -23,7 +26,6 @@
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
             $page = $_GET['page'] ?? 'home';
             require_once __DIR__ . '/components/navbar.php';
-
         } else {
             $page = $_GET['page'] ?? 'landing';
         }
@@ -31,21 +33,34 @@
         switch ($page) {
             case 'landing':
                 require_once __DIR__ . '/src/pages/welcome.php';
-                echo "<title>SODA | Welcome</title>";
+                title('Welcome');
                 break;
             case 'login':
                 require_once __DIR__ . '/src/users/login.php';
+                title('Login');
                 break;
             case 'home':
                 require_once __DIR__ . '/src/users/index.php';
+                title('Home');
                 break;
             case 'listings':
                 require_once __DIR__.'/src/listing/index.php';
+                title("Listings");
                 break;
             case 'messages':
                 require_once __DIR__.'/src/message/index.php';
+                title("Messages");
+                break;
+            case 'template':
+                require_once __DIR__.'/src/template/template.php';
+                break;
+        }
+        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
+            require_once __DIR__ . '/components/dashboard.php';
+
         }
         require_once __DIR__ . '/components/waves.php';
+        require_once __DIR__.'/src/request/index.php';
         ?>
     </div>
 
