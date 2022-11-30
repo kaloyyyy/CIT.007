69ff33791c20668798acc19576eb46f1928978e5
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-11-30 13:04:04 +0800

    request created

---
+++ komiV2/src/request/post-request.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . "/../../config/config.php";
+session_start();
+$desc = $_POST['desc'];
+$price = $_POST['price'];
+$datetime = $_POST['deadline'];
+$currID = $_SESSION['id'];
+
+
+$query = "INSERT INTO komision.postrequest (postID, userID, Description, price, date_created, deadline) 
+            VALUES (DEFAULT, $currID, '$desc', $price, DEFAULT, '$datetime');";
+$mysqli->query($query);
