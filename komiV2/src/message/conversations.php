3683ac97d042360d5db8ea9c864afc82192dcb26
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-12-01 21:39:57 +0800

    message update

--- komiV2/src/message/conversations.php
+++ komiV2/src/message/conversations.php
@@ -1,21 +1,34 @@
-<?php
-if (!isset($_SESSION)) {
-    session_start();
-}
+<div class="col-4 h-100 border-right post-border p-0">
+    <?php
+    if (!isset($_SESSION)) {
+        session_start();
+    }
 
-require_once __DIR__.'/../../config/config.php';
-$id = $_SESSION['id'];
-$accType = $_SESSION['userType'];
-if ($accType == 0) {
-    $myType = 'userID';
-    $conversationQuery = "select * from convo left join freelancers f on f.freelancerID = convo.fl_userID left join users u on u.userID = f.freelancerID where convo.userID = $id";
-} else {
-    $myType='fl_userID';
-    $conversationQuery = "select * from convo left join users u on u.userID = convo.userID where convo.fl_userID = $id";
+    require_once __DIR__.'/../../config/config.php';
+    $id = $_SESSION['id'];
+    $accType = $_SESSION['userType'];
+    if ($accType == 0) {
+        $myType = 'userID';
+        $conversationQuery = "select * from convo left join freelancers f on f.freelancerID = convo.fl_userID left join users u on u.userID = f.freelancerID where convo.userID = $id";
+    } else {
+        $myType='fl_userID';
+        $conversationQuery = "select * from convo left join users u on u.userID = convo.userID where convo.fl_userID = $id";
 
-}
-$conversationRes = $mysqli->query($conversationQuery);
-
-foreach ($conversationRes as $rows){
-    echo $rows['username']."<br>";
-}
+    }
+    $conversationRes = $mysqli->query($conversationQuery);
+    foreach ($conversationRes as $rows){
+        $convoID = $rows['convoID'];
+        $lastChatQuery = "select message from chat where convID = $convoID";
+        $lastChatRes = $mysqli->query($lastChatQuery);
+        $lastChatRow = mysqli_fetch_array($lastChatRes);
+        echo "<a class='' href='/?page=messages&convo=$convoID'><div class=' p-0 m-0'>";
+        echo "<div class=''>";
+        echo $rows['username'];
+        echo "</div>";
+        echo "<div class='text-secondary'>";
+        echo $lastChatRow[0];
+        echo "</div>";
+        echo "</div></a>";
+    }
+    ?>
+</div>
--- komiV2/src/message/index.php
+++ komiV2/src/message/index.php
@@ -1,7 +1,10 @@
 <div class="col-6  post-border border-left border-right  p-0 m-0">
-    <div class="container p-0">
+    <?php
+    require_once __DIR__.'/../../components/header.php';
+    ?>
+    <div class="container p-0 d-flex h-100 border-top post-border">
         <?php
-        require_once __DIR__.'/../../components/header.php';
+
         require_once __DIR__.'/conversations.php';
         require_once __DIR__.'/chats.php';
         ?>
