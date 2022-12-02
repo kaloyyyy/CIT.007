4c2776f2cc4459a9dc146e5815d09c43ed870d3e
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-12-03 03:21:44 +0800

    Update icon.php

--- komiV2/components/icon.php
+++ komiV2/components/icon.php
@@ -1,21 +1,22 @@
 <?php
 /**
  * @param mixed $id
+ * @param mixed $dim
  * @return string
  */
-function getImg(mixed $id): string
+function getImg(mixed $id, mixed $dim): string
 {
     $file = '/src/uploads/' . $id . '.jpg';
     $file2 = '/src/uploads/' . $id . '.png';
     if (!(file_exists(__DIR__ . '/../' . $file))) {
         if (!(file_exists(__DIR__ . '/../' . $file2))) {
-            $icon = "<i class='fa-solid fa-user' style='height: 1.8em'></i>";
+            $icon = "<i class='fa-solid fa-user' style='height: 1.5em'></i>";
         } else {
             $file = $file2;
-            $icon = "<img src = $file alt='' class='rounded-pill'  style='width: 2.3em; height: 2.3em;'>";
+            $icon = "<img src = $file alt='' class='rounded-pill'  style='width: $dim; height: $dim; object-fit: cover'>";
         }
     } else {
-        $icon = "<img src = $file alt='' class='rounded-pill'  style='width: 2.3em; height: 2.3em;'>";
+        $icon = "<img src = $file alt='' class='rounded-pill'  style='width:$dim; height: $dim; object-fit: cover'>";
     }
     return $icon;
 }
