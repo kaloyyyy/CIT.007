6381ae098875ba272871387157f9bedd9f64b655
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-12-03 03:21:32 +0800

    Update header.php

--- komiV2/components/header.php
+++ komiV2/components/header.php
@@ -5,14 +5,14 @@ if (isset($currentPage)) {
     if ($currentPage == 'message') {
 
     }else{
-        echo "<div class='d-flex flex-row navbar komi-header align-items-center py-1' style = 'height: 5vh'>
+        echo "<div class='d-flex flex-row navbar komi-header align-items-center py-1' style = 'height: 2.5em'>
             <h5 class='align-items-center d-flex'>";
         echo $currentPage;
         echo "</h5></div>";
     }
 
 } else {
-    echo "<div class='d-flex flex-row navbar komi-header align-items-center py-1 ' style='height: 5vh'>
+    echo "<div class='d-flex flex-row navbar komi-header align-items-center py-1 ' style='height: 2.5em'>
             <h5 class='align-items-center'>";
     echo "Komision";
     echo "</h5></div>";
