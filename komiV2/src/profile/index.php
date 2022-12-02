35302873a1039108b5c01e44cb7ebb1313b54dbb
Kerbsby <91007525+Kerbsby@users.noreply.github.com>
2022-12-03 00:35:49 +0800

    d

--- komiV2/src/profile/index.php
+++ komiV2/src/profile/index.php
@@ -9,23 +9,6 @@ require_once __DIR__ . "/../../config/lib.php";
 // Check if the user is logged in, if not then redirect him to login page
 
 
-
-if(isset($_POST['sub'])) {
-    $id = $_GET['update'];
-    $fullname = $_POST['dFullName'];
-
-
-
-    $sql = "UPDATE users SET userID = '$id', username = '$fullname' WHERE userID = '$id'";
-    $result = mysqli_query($mysqli, $sql);
-    if ($result) {
-
-        echo '<script type="text/javascript">window.top.location="/src/users/logout.php";</script>';
-
-    }
-
-
-}
 ?>
 
 
@@ -54,140 +37,10 @@ while ($row = mysqli_fetch_array($result)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/bf1c643ee2.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/Web_Tech/css/style.css">
+    <link rel="stylesheet" href="/komiV2/src/profile/style.css">
     <title>MedBook</title>
 </head>
 <style>
-    .panel {
-        margin-bottom: 23px;
-        background-color: #ffffff;
-        border: 1px solid transparent;
-        border-radius: 3px;
-        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
-    }
-    .panel-body {
-        padding: 15px;
-    }
-    .panel-heading {
-        padding: 10px 15px;
-        border-bottom: 1px solid transparent;
-        border-top-right-radius: 2px;
-        border-top-left-radius: 2px;
-    }
-    .panel-heading > .dropdown .dropdown-toggle {
-        color: inherit;
-    }
-    .panel-title {
-        margin-top: 0;
-        margin-bottom: 0;
-        font-size: 15px;
-        color: inherit;
-    }
-    .panel-title > a,
-    .panel-title > small,
-    .panel-title > .small,
-    .panel-title > small > a,
-    .panel-title > .small > a {
-        color: inherit;
-    }
-    .panel > .list-group,
-    .panel > .panel-collapse > .list-group {
-        margin-bottom: 0;
-    }
-    .panel > .list-group .list-group-item,
-    .panel > .panel-collapse > .list-group .list-group-item {
-        border-width: 1px 0;
-        border-radius: 0;
-    }
-    .panel > .list-group:first-child .list-group-item:first-child,
-    .panel > .panel-collapse > .list-group:first-child .list-group-item:first-child {
-        border-top: 0;
-        border-top-right-radius: 2px;
-        border-top-left-radius: 2px;
-    }
-    .panel > .list-group:last-child .list-group-item:last-child,
-    .panel > .panel-collapse > .list-group:last-child .list-group-item:last-child {
-        border-bottom: 0;
-        border-bottom-right-radius: 2px;
-        border-bottom-left-radius: 2px;
-    }
-    .panel > .panel-heading + .panel-collapse > .list-group .list-group-item:first-child {
-        border-top-right-radius: 0;
-        border-top-left-radius: 0;
-    }
-    .panel-heading + .list-group .list-group-item:first-child {
-        border-top-width: 0;
-    }
-    .list-group + .panel-footer {
-        border-top-width: 0;
-    }
-
-
-    .panel-primary > .panel-heading {
-        color: #ffffff;
-        background-color: #2196f3;
-    }
-    .panel-primary > .panel-heading + .panel-collapse > .panel-body {
-        border-top-color: #2196f3;
-    }
-    .panel-primary > .panel-heading .badge {
-        color: #2196f3;
-        background-color: #ffffff;
-    }
-    .panel-primary > .panel-footer + .panel-collapse > .panel-body {
-        border-bottom-color: #2196f3;
-    }
-
-
-    .upload{
-        width: 140px;
-        position: relative;
-        margin: auto;
-        text-align: center;
-    }
-    .upload img{
-        border-radius: 50%;
-        border: 8px solid #DCDCDC;
-        width: 125px;
-        height: 125px;
-    }
-    .upload .rightRound{
-        position: absolute;
-        bottom: 0;
-        right: 0;
-        background: #00B4FF;
-        width: 32px;
-        height: 32px;
-        line-height: 33px;
-        text-align: center;
-        border-radius: 50%;
-        overflow: hidden;
-        cursor: pointer;
-    }
-    .upload .leftRound{
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        background: red;
-        width: 32px;
-        height: 32px;
-        line-height: 33px;
-        text-align: center;
-        border-radius: 50%;
-        overflow: hidden;
-        cursor: pointer;
-    }
-    .upload .fa{
-        color: white;
-    }
-    .upload input{
-        position: absolute;
-        transform: scale(2);
-        opacity: 0;
-    }
-    .upload input::-webkit-file-upload-button, .upload input[type=submit]{
-        cursor: pointer;
-    }
 
 </style>
 <body>
@@ -310,7 +163,6 @@ while ($row = mysqli_fetch_array($result)) {
                                         <h5> <strong> Client </strong></h5>
 
                                         <hr />
-                                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#doctorprofile"><a class="glyphicon glyphicon-edit" style="text-decoration: none;" href="?update=<?php echo $row['userID']; ?>" title="Edit">Update Profile</a></button>
                                     </div>
                                 </div>
                             </div>
@@ -359,40 +211,6 @@ while ($row = mysqli_fetch_array($result)) {
 
 
 
-<div class="modal fade" id="doctorprofile" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel">Update Profile</h4>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <!-- form start -->
-
-                <form action="" method="post" >
-                    <table class="table table-user-information">
-                        <tbody>
-                        <tr>
-                            <td>Doctor FullName</td>
-                            <td><input type="text" name="dFullName" value="" id="dfullname" class="form-control input-lg" placeholder="Full Name" required /></td>
-                        </tr>
-                        </tbody>
-                    </table>
-
-                    <div class="modal-footer">
-                        <button type="submit" class="btn" name="sub" id="sub" >Update</button>
-                        <button type="submit" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-                    </div>
-                </form>
-
-                <!-- form end -->
-            </div>
-
-        </div>
-    </div>
-</div>
-<br/><br/>
-</div>
 
 
 
