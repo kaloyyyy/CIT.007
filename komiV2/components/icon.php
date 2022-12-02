cb560d292060cf99cefd99c7ee8c3f96e0c1bfc2
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-12-03 02:37:29 +0800

    Update icon.php

--- komiV2/components/icon.php
+++ komiV2/components/icon.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * @param mixed $id
+ * @return string
+ */
+function getImg(mixed $id): string
+{
+    $file = '/src/uploads/' . $id . '.jpg';
+    $file2 = '/src/uploads/' . $id . '.png';
+    if (!(file_exists(__DIR__ . '/../' . $file))) {
+        if (!(file_exists(__DIR__ . '/../' . $file2))) {
+            $icon = "<i class='fa-solid fa-user' style='height: 1.8em'></i>";
+        } else {
+            $file = $file2;
+            $icon = "<img src = $file alt='' class='rounded-pill'  style='width: 2.3em; height: 2.3em;'>";
+        }
+    } else {
+        $icon = "<img src = $file alt='' class='rounded-pill'  style='width: 2.3em; height: 2.3em;'>";
+    }
+    return $icon;
+}
+
+
