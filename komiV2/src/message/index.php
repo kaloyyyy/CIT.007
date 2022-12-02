dcfa5e80f3449d2b9e4b5e435bd1e5bb4bfadff3
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-12-03 02:37:46 +0800

    Update index.php

--- komiV2/src/message/index.php
+++ komiV2/src/message/index.php
@@ -1,7 +1,5 @@
 <div class="col-6  post-border border-left border-right  p-0 m-0 ">
-    <?php
-    require_once __DIR__.'/../../components/header.php';
-    ?>
+
     <div class="container p-0 d-flex border-top post-border translucent" style="height: 95vh">
         <?php
 
