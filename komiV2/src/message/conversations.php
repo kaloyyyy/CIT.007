e805de1ea26c10713787639c2f5c3d67c900a998
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-12-03 03:21:51 +0800

    Update conversations.php

--- komiV2/src/message/conversations.php
+++ komiV2/src/message/conversations.php
@@ -7,6 +7,7 @@
     }
     $activeConvo = $_GET['convo'] ?? '';
     require_once __DIR__.'/../../config/config.php';
+    require_once __DIR__.'/../../components/icon.php';
     $id = $_SESSION['id'];
     $accType = $_SESSION['userType'];
     if ($accType == 0) {
@@ -15,21 +16,32 @@
     } else {
         $myType='fl_userID';
         $conversationQuery = "select * from convo left join users u on u.userID = convo.userID where convo.fl_userID = $id";
-
     }
     $conversationRes = $mysqli->query($conversationQuery);
     foreach ($conversationRes as $rows){
         $convoID = $rows['convoID'];
+        if($convoID == $activeConvo){
+            $activeUser=  $rows['username'];
+        }
+        $currentID = $rows['userID'];
         $lastChatQuery = "select message from chat where convID = $convoID order by created_at desc ";
         $lastChatRes = $mysqli->query($lastChatQuery);
         $lastChatRow = mysqli_fetch_array($lastChatRes);
-        echo "<a class='btn btn-chat  text-left w-100 chat-link' id='$convoID' href='/?page=Messages&convo=$convoID'><div class=' p-0 m-0'>";
-        echo "<div class='' style='font-size: 1.1rem'>";
-        echo $rows['username'];
-        echo "</div>";
-        echo "<div class='text-secondary text-truncate'>";
-        echo $lastChatRow[0];
-        echo "</div>";
+        echo "<a class='btn btn-chat text-left w-100 chat-link m-0 p-1' id='$convoID' href='/?page=Messages&convo=$convoID'><div class=' p-0 m-0'>";
+            echo "<div class='d-flex align-items-center'>";
+                echo "<div class='d-flex align-items-center justify-content-center m-1 rounded-pill active' style='min-width: 3rem; min-height: 3rem'>";
+                $img = getImg($currentID, '2.5em');
+                echo $img;
+                echo "</div>";
+                echo "<div class='d-block'  style='width: 100%'>";
+                    echo "<div class='' style='font-size: 1.1rem'>";
+                        echo $rows['username'];
+                    echo "</div>";
+                    echo "<div class='text-secondary text-truncate' style='min-width: 0; max-width: 80%'>";
+                        echo $lastChatRow[0];
+                    echo "</div>";
+                echo "</div>";
+            echo "</div>";
         echo "</div></a>";
     }
     ?>
