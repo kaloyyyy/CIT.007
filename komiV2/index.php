4e07c327c374fb9e32d314d1f8d093b6d11caaff
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-12-06 13:03:08 +0800

    routing and style update

--- komiV2/index.php
+++ komiV2/index.php
@@ -17,6 +17,7 @@
         {
             echo "<title>Komision | $title</title>";
         }
+
         if (!isset($_SESSION)) {
             session_start();
         }
@@ -25,42 +26,52 @@
 
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
             $page = $_GET['page'] ?? 'Home';
-            require_once __DIR__ . '/components/navbar.php';
+            if ($page != 'Home') {
+                require_once __DIR__ . '/components/navbar.php';
+            }
             $userType = $_SESSION['userType'];
         } else {
             $page = $_GET['page'] ?? 'Landing';
         }
-
+        if(!isset($_SESSION['username'])){
+            $page = "Landing";
+        }
         switch ($page) {
             case 'Landing':
                 require_once __DIR__ . '/src/pages/welcome.php';
                 title('Welcome');
                 break;
             case 'Login':
-                header("location: /src/users/login.php");
+                require_once __DIR__.'/src/users/login.php';
                 break;
             case 'Home':
-                require_once __DIR__ . '/src/users/index.php';
+                require_once __DIR__ . '/src/pages/welcome.php';
                 title('Home');
                 break;
             case 'Listings':
-                require_once __DIR__.'/src/listing/index.php';
+                require_once __DIR__ . '/src/listing/index.php';
                 title("Listings");
                 break;
             case 'Messages':
-                require_once __DIR__.'/src/message/index.php';
+                require_once __DIR__ . '/src/message/index.php';
                 title("Messages");
                 break;
-            case 'template':
-                require_once __DIR__.'/src/template/template.php';
+            case 'Template':
+                require_once __DIR__ . '/src/template/template.php';
+                break;
+            case 'User':
+                require_once __DIR__ . '/src/profile/index.php';
                 break;
         }
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
-            require_once __DIR__ . '/components/dashboard.php';
+            if ($page != 'Home') {
+                require_once __DIR__ . '/components/dashboard.php';
+            }
+
 
         }
         require_once __DIR__ . '/components/waves.php';
-        require_once __DIR__.'/src/request/index.php';
+        require_once __DIR__ . '/src/request/index.php';
         ?>
     </div>
 
