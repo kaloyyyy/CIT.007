ab3b7528fb6c503d15f506bf84b01c7a726b6b7d
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-12-07 00:25:16 +0800

    Delete doctor directory

--- doctor/db/doctor.sql
+++
@@ -1,4 +0,0 @@
-insert into doc.doctor (d_id, given_name, sur_name)
-values  (1, 'Juan', 'Dela Cruz'),
-        (2, 'Quack', 'Duck'),
-        (3, 'Jeff', 'Bezos');
--- doctor/db/profile.sql
+++
@@ -1,2 +0,0 @@
-insert into doc.profile (p_id, given_name, sur_name, weight, height, gender)
-values  (4, 'Bean', 'Snow', 603, 120, 'male');
